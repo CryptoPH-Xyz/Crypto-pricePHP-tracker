b8b8e1644c0a338413686449c49fffb392baf258
Xyz Fiegalan <xyzfiegalan@gmail.com>
2021-05-23 19:46:23 +0800

    homepage, destructure coin

---
+++ components/Coin.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function Coin(props) {
+    return (
+        <div className='coin-container'>
+        <div className='coin-row'>
+          <div className='coin'>
+            <img src={props.image} alt='crypto' />
+            <h1>{props.name}</h1>
+            <p className='coin-symbol'>{props.symbol.toUpperCase()}</p>
+          </div>
+          <div className='coin-data'>
+            <p className='coin-price'>₱{props.price.toLocaleString()}</p>
+  
+            {props.priceChange1h < 0 ? (
+              <p className='coin-percent red'>{props.priceChange1h.toFixed(2)}%</p>
+            ) : (
+              <p className='coin-percent green'>{props.priceChange1h.toFixed(2)}%</p>
+            )}
+
+            {props.priceChange24h < 0 ? (
+              <p className='coin-percent red'>{props.priceChange24h.toFixed(2)}%</p>
+            ) : (
+              <p className='coin-percent green'>{props.priceChange24h.toFixed(2)}%</p>
+            )}
+
+            {props.priceChange7d < 0 ? (
+              <p className='coin-percent red'>{props.priceChange7d.toFixed(2)}%</p>
+            ) : (
+              <p className='coin-percent green'>{props.priceChange7d.toFixed(2)}%</p>
+            )}
+
+            <p className='coin-volume'>₱{props.volume.toLocaleString()}</p>
+  
+            <p className='coin-marketcap'>
+              Mkt Cap: ₱{props.marketCap.toLocaleString()}
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,63 @@
 import Head from 'next/head'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import Coin from '../components/Coin';
 
 export default function Home() {
 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=php&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d')
     .then(res => {
       setCoins(res.data);
-    })
-  })
+      console.log(res.data);
+    }).catch(error => console.log(error));
+  }, []);
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <>
       <Head>
         <title>Crypto Price Tracker</title>
       </Head>
-      <h1>APIII</h1>
+      <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            marketCap={coin.market_cap}
+            image={coin.image}
+            priceChange1h={coin.price_change_percentage_1h_in_currency}
+            priceChange24h={coin.price_change_percentage_24h_in_currency}
+            priceChange7d={coin.price_change_percentage_7d_in_currency}
+          />
+        );
+      })}
+    </div>
     </>
   )
 }
